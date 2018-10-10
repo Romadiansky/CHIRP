c0347cd317055c4d53ca1903f39bb50644e4b55f
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-10 19:26:52 +0000

    created an app.js function to convert json data into HTML

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+var $tweet = createTweetElement(tweetData)
+
+function createTweetElement(tweetData) {
+
+  return `<article>
+    <header>
+      <h2>${tweetData.user.name}</h2>
+      <img src ="${tweetData.user.avatars.large}"/>
+      <span class="handle">${tweetData.user.handle}</span>
+    <section class="tweetText">${tweetData.content.text}</section>
+    <footer>
+      <span class="days"${tweetData.created_at}></span>
+    </footer>
+  </article>`;
+}
+
+console.log($tweet);
+$('#restTweets').append($tweet);
+
+function renderTweets() {
+
+}
