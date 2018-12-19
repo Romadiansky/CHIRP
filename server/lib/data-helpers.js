dcdf68e7887a6033bd552d0cb95c2168c48366db
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-12-19 00:22:31 +0000

    completed new feature for app that allows users to express how they feel about the anonymous tweets of others using one of three icons; each icon is explained on hover; each icon also have an additional icon layered overtop that keep count of how many times it's been clicked and stores it to the database

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,4 +1,5 @@
 "use strict";
+const ObjectId = require("mongodb").ObjectId;
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -8,6 +9,17 @@ module.exports = function makeDataHelpers(db) {
     saveTweet: function(newTweet, callback) {
       db.collection('tweets').insertOne(newTweet);
       callback(null, true);
+    },
+
+    // Updates a tweet by incrementing the correct vote
+    updateTweet: function(obj, callback) {
+      db.collection('tweets').findAndModify(
+        { _id: new ObjectId(obj.id) },
+        [['_id','asc']],
+        { $inc: {[obj.icon]: 1} },
+        {new: true},
+        callback
+      );
     },
 
     // Get all tweets in `db`, sorted by newest first
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -31,6 +31,9 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
+      icon1: 0,
+      icon2: 0,
+      icon3: 0,
       created_at: Date.now()
     };
 
@@ -39,6 +42,17 @@ module.exports = function(DataHelpers) {
         res.status(500).json({ error: err.message });
       } else {
         res.status(201).send();
+      }
+    });
+  });
+
+  // accepts a new tweet vote
+  tweetsRoutes.post("/vote", function(req, res) {
+    DataHelpers.updateTweet(req.body, (err, result) => {
+      if (err) {
+        res.status(500).json({error: err.message});
+      } else {
+        res.json(result);
       }
     });
   });
